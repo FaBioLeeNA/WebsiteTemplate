4531c3ab9bd6e14be0a4b0e4e30b1d582629f4ec
Fabian Yee <fabianyee952@gmail.com>
2021-05-28 13:03:08 -0700

    added link to logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,9 +14,9 @@ const NavBar = () => {
         <div class="row">
           <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
             <div class="logo">
-              <a href="index.html">
+              <Link to="/">
                 <img src={logo} alt="img" />
-              </a>
+              </Link>
             </div>
           </div>
           <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
